091cd7fad2f475bf521d638f8fc068ec016b38d2
Elli Raynai <elli@elliraynai.com>
2021-07-11 08:56:31 -0400

    push starting files

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('/', views.starting_page),
+    path('posts/', views.posts),
+    path('posts/<slug:slug>', views.single_post),
+]
